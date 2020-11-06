4898fe2eff611c62a1e79dc5f50dc07faf11a577
Abdulaziz Sadi <57579000+kitindi@users.noreply.github.com>
2020-11-06 16:32:29 +0300

    created link to database and render data into browser

--- add.php
+++ add.php
@@ -1,43 +1,58 @@
 <?php 
-  //check if form has data to be sent to the server
 
-// if(isset($_GET['submit'])){
-// 	echo $_GET['email'];
-// 	echo $_GET['title'];
-// 	echo $_GET['ingridients'];
+$email='';
+$title='';
+$ingredients='';
 
-// }
-// using htmlspecialchars prevents XSS (Cross Sites Scripting) attacks!
+$errors=['email'=>'','title'=>'','ingredients'=>''];
 
-if(isset($_POST['submit'])){
-// 	echo htmlspecialchars($_POST['email']);
-// 	echo htmlspecialchars($_POST['title']);
-// 	echo htmlspecialchars($_POST['ingridients']);
-// }
+ if(isset($_POST['submit'])){
+ 	// echo htmlspecialchars($_POST['email']);
+ 	// echo htmlspecialchars($_POST['title']);
+ 	// echo htmlspecialchars($_POST['ingridients']);
 
-## Basic Validation_1
-//check email 
-	if(empty($_POST['email'])){
-		echo "An email is required<br>";
-	}else{
-		echo htmlspecialchars($_POST['email']);
-	}
+ 	#Check email
+  if(empty($_POST['email'])){
+  	$errors['email']= "An email is required! <br>";
+  }else{
+  	$email=$_POST['email'];
+    if(!filter_var($email,FILTER_VALIDATE_EMAIL)){
+    	$errors['email']= "Email must be a valid email address";
+    }
+  };
 
-	//check title
+ #Check Title field
+if(empty($_POST['title'])){
+  $errors['title']="A title is required! <br>";
+  }else{
+  	$title= $_POST['title'];
+  	if(!preg_match('/^[a-zA-Z\s]+$/', $title)){
+  		$errors['title']='Title must be letters and spaces only.';
+  	}
+  };
 
-	if(empty($_POST['title'])){
-		echo "A title is required <br>";
-	}else{
-		echo htmlspecialchars($_POST['email']);
-	}
-	// ckeck ingridients
-	if(empty($_POST['ingridients'])){
-		echo "At least one ingridient is required<br>";
-	}else{
-		echo htmlspecialchars($_POST['email']);
-	}
-//  end of POST checks
-}
+ #Check Ingiridients
+if(empty($_POST['ingredients'])){
+  $errors['ingredients']="Ingredients are required separated by comma! <br>";
+  }else{
+  	$ingridients=$_POST['ingredients'];
+  	if(!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)){
+  		$errors['ingredients']='Ingredients must be a comma separated list';
+  	}
+  }
+
+
+#check if there are any errors before redirecting
+
+  if(array_filter($errors)){
+  	// echo "THere are errors in a form";
+  }else{
+  	// echo 'form is valid';
+#code to redirect user to the different page
+  	header('location:index.php');
+  }
+
+ }// End of POST check
 
 ?>
 
@@ -49,11 +64,20 @@ if(isset($_POST['submit'])){
  	<h4 class="center">Add a Pizza</h4>
  	<form class="white" action="add.php" method="POST">
  		<label>Your Email</label>
- 		<input type="text" name="email">
+ 		<input type="text" name="email" value="<?php echo htmlspecialchars($email);?>">
+ 		<div class="red-text">
+ 		 <?php echo $errors['email']?>
+ 		</div>
  		<label>Pizza Title</label>
- 		<input type="text" name="title">
- 		<label>Ingridients(comma separated)</label>
- 		<input type="text" name="ingridients">
+ 		<input type="text" name="title" value="<?php echo htmlspecialchars($title);?>">
+ 		<div class="red-text">
+ 		<?php echo $errors['title']?>
+ 		</div>
+ 		<label>Ingredients(comma separated)</label>
+ 		<input type="text" name="ingredients"  value="<?php echo htmlspecialchars($ingredients);?>">
+ 		<div class="red-text">
+ 		<?php echo $errors['ingredients']?>
+ 		</div>
  		<div class="center">
  			<input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
  		</div>
--- index.php
+++ index.php
@@ -1,4 +1,33 @@
 <?php 
+//MySQLi or PDO{PHP Data Objects}
+//connect to database using mysqli(databasehost, username,password, databasename)
+
+$conn =mysqli_connect('localhost','bongo','bongo123','bongo_pizza');
+
+//check the connection
+
+if(!$conn){
+ echo "Connection error: ".mysqli_connect_error();
+}
+
+//write query for all pizzas
+
+$sql ='SELECT title, ingredients, id FROM pizzas ORDER BY created_at';
+
+//make query and get results
+
+$results =mysqli_query($conn, $sql);
+
+//fetch the resulting rows as an array
+
+$pizzas = mysqli_fetch_all($results,MYSQLI_ASSOC);
+
+//free the results from memmory
+mysqli_free_result($results);
+//close the connection
+
+mysqli_close($conn);
+
 
 
 ?>
@@ -6,6 +35,30 @@
 <!DOCTYPE html>
 <html>
 <?php include('templates/header.php');?>
+ 
+ <h4 class="center grey-text">Pizzas</h4>
+ <div class="container">
+ 	<div class="row">
+ 		<?php foreach ($pizzas as $key => $pizza) {?>
+
+ 			<div class="col s6 md3">
+ 				<div class="card z-depth-0">
+ 					<div class="card-content center">
+ 						<h6><?php echo htmlspecialchars($pizza['title']);?></h6>
+ 						<div><?php  echo htmlspecialchars($pizza['ingredients'])?></div>
+ 					</div>
+ 					<div class="card-action right-align">
+ 					<a href="#" class="brand-text">more info</a>
+ 				</div>
+ 				</div>
+ 				
+ 			</div>
+
+ 		<?php } ?>
+ 		
+ 	</div>
+ </div>
+
 <?php include('templates/footer.php');?>
 
 </html>
