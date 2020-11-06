71046292b819d75481ab3a407b19b4660e5b1009
Abdulaziz Sadi <57579000+kitindi@users.noreply.github.com>
2020-11-06 03:59:16 +0300

    project starter

---
+++ templates/header.php
@@ -0,0 +1,24 @@
+<head>
+	<title>Bongo Pizza</title>
+	 <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+   <style type="text/css">
+   	.brand{
+   		background: #D99880 !important;
+   	}
+   	.brand-text{
+   		color:#31ABC9 !important;
+   		text-align: left;
+   		font-weight: 700;
+   	}
+   </style>
+</head>
+<body class="gray lighten-4">
+	<nav class="white lighten-2">
+		<div class="container">
+			<a href="#" class="brand-logo brand-text">Bongo Pizza</a>
+			<ul id="nav-mobile" class="right hide-on-small-and-down">
+				<li><a href="#" class="btn brand z-depth-0">ADD A PIZZA</a></li>
+			</ul>
+		</div>
+	</nav>
